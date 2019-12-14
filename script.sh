a2db76c1d402f8c784994c47289d07914c6dc1ff
swathigoudar <swathigoudar@gmail.com>
2019-12-14 12:40:25 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
-sudo docker stop $(sudo docker ps -a -q)
-sudo docker rm $(sudo docker ps -a -q)
-cd /home/ubuntu
-rm -rf dockerImageNode
-git clone https://github.com/andalike/dockerImageNode.git
-cd dockerImageNode
-sudo docker build -t andalike/node-web-app .
-sudo docker run -p 49160:8080 -d andalike/node-web-app
+cd cicdconfig
+aws s3 sync . s3://cicdconfig123
+rm -rf cicdconfig
+git clone https://github.com/swathigoudar/cicdconfig.git
+cd cicdconfig
